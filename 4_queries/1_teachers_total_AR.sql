b89bd90f6e8b6e7bd45cc9df9ddee8f57a7da7e0
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-28 23:58:45 +0000

    Added queries that include teachers and assistance requests

---
+++ 4_queries/1_teachers_total_AR.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(assistance_requests.*) as total_assistances, 
+  teachers.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY name;
