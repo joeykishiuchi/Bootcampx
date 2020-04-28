cab6876b3b0fd26e7a164a3ed1463249f660fe43
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-28 02:33:15 +0000

    Added assignments and submissions tables and initialized them with data

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,10 +1,3 @@
-CREATE TABLE cohorts (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  start_date DATE,
-  end_date DATE
-);
-
 CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -14,4 +7,11 @@ CREATE TABLE students (
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
 );
